810f77f39038bd9bcc63c7ed2accfd214a9a6abc
whxogus215 <whxogus215@naver.com>
2023-08-10 09:29:33 +0900

    백엔드 구축

--- FullStackTutorial/src/main/java/net/javaguides/springboot/FullStackTutorialApplication.java
+++ FullStackTutorial/src/main/java/net/javaguides/springboot/FullStackTutorialApplication.java
@@ -1,13 +1,44 @@
 package net.javaguides.springboot;
 
+import net.javaguides.springboot.entity.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FullStackTutorialApplication {
+public class FullStackTutorialApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(FullStackTutorialApplication.class, args);
     }
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Employee employee1 = Employee.builder()
+                .firstName("Ramesh")
+                .lastName("Fadatare")
+                .email("ramesh@gmail.com")
+                .build();
+
+        Employee employee2 = Employee.builder()
+                .firstName("Tony")
+                .lastName("Stark")
+                .email("tony@gmail.com")
+                .build();
+
+        Employee employee3 = Employee.builder()
+                .firstName("John")
+                .lastName("Cena")
+                .email("cena@gmail.com")
+                .build();
+
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+        employeeRepository.save(employee3);
+    }
 }
--- FullStackTutorial/src/main/java/net/javaguides/springboot/entity/Employee.java
+++ FullStackTutorial/src/main/java/net/javaguides/springboot/entity/Employee.java
@@ -1,8 +1,25 @@
 package net.javaguides.springboot.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "employees")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name") // 테이블에 열 이름을 지정할 때, 두 단어가 합쳐있을 경우 _로 구분하기 위해 @Column으로 직접 정의하였다.
     private String lastName;
     private String email;
 }
