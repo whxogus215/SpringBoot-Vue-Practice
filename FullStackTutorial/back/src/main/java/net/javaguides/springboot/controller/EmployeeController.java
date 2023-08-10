4675d270527affe5e99dae2934fd4423d7d5aa9a
whxogus215 <whxogus215@naver.com>
2023-08-10 11:17:41 +0900

    CORS 문제 해결

--- FullStackTutorial/back/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ FullStackTutorial/back/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package net.javaguides.springboot.controller;
 import net.javaguides.springboot.entity.Employee;
 import net.javaguides.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:8081/")
 public class EmployeeController {
 
     @Autowired
